cfc4a1fc4ae809948184cd4710da6b022c68e309
Adrien Boutigny <adrien@slash-tmp.dev>
2022-07-27 14:59:23 +0200

    squash migrations into a single migration file

--- confiture-rest-api/prisma/migrations/20220727125858_add_audit_step_one_fields/migration.sql
+++ confiture-rest-api/prisma/migrations/20220727125858_add_audit_step_one_fields/migration.sql
@@ -1,24 +1,33 @@
 /*
   Warnings:
 
-  - Added the required column `administration` to the `Audit` table without a default value. This is not possible if the table is not empty.
+  - You are about to drop the column `uniqueId` on the `Audit` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[editUniqueId]` on the table `Audit` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[consultUniqueId]` on the table `Audit` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `auditorEmail` to the `Audit` table without a default value. This is not possible if the table is not empty.
   - Added the required column `auditorName` to the `Audit` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `consultUniqueId` to the `Audit` table without a default value. This is not possible if the table is not empty.
   - Added the required column `contactEmail` to the `Audit` table without a default value. This is not possible if the table is not empty.
   - Added the required column `contactFormUrl` to the `Audit` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `ministry` to the `Audit` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `editUniqueId` to the `Audit` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `initiator` to the `Audit` table without a default value. This is not possible if the table is not empty.
   - Added the required column `url` to the `Audit` table without a default value. This is not possible if the table is not empty.
   - Made the column `procedure` on table `Audit` required. This step will fail if there are existing NULL values in that column.
 
 */
+-- DropIndex
+DROP INDEX "Audit_uniqueId_key";
+
 -- AlterTable
-ALTER TABLE "Audit" ADD COLUMN     "administration" TEXT NOT NULL,
+ALTER TABLE "Audit" DROP COLUMN "uniqueId",
 ADD COLUMN     "auditorEmail" TEXT NOT NULL,
 ADD COLUMN     "auditorName" TEXT NOT NULL,
+ADD COLUMN     "consultUniqueId" TEXT NOT NULL,
 ADD COLUMN     "contactEmail" TEXT NOT NULL,
 ADD COLUMN     "contactFormUrl" TEXT NOT NULL,
 ADD COLUMN     "contactName" TEXT,
-ADD COLUMN     "ministry" TEXT NOT NULL,
+ADD COLUMN     "editUniqueId" TEXT NOT NULL,
+ADD COLUMN     "initiator" TEXT NOT NULL,
 ADD COLUMN     "url" TEXT NOT NULL,
 ALTER COLUMN "procedure" SET NOT NULL;
 
@@ -31,6 +40,12 @@ CREATE TABLE "Recipent" (
 
     CONSTRAINT "Recipent_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Audit_editUniqueId_key" ON "Audit"("editUniqueId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Audit_consultUniqueId_key" ON "Audit"("consultUniqueId");
 
 -- AddForeignKey
 ALTER TABLE "Recipent" ADD CONSTRAINT "Recipent_auditId_fkey" FOREIGN KEY ("auditId") REFERENCES "Audit"("id") ON DELETE SET NULL ON UPDATE CASCADE;
